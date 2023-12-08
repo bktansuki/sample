922539a1a57d0761bec024f0ffefe63791b665d8
bktan <tanboonkan@gmail.com>
2023-12-08 17:32:59 +0800

    multuply in class-amend

--- project01/sample.cc
+++ project01/sample.cc
@@ -5,6 +5,7 @@ namespace sample {
 
 int Sample::AddNumbers(int a, int b) { return a + b; }
 int Sample::MinusNumbers(int a, int b) { return a - b; }
+int Sample::MultiplyNumbers(int a, int b) { return a * b; }
 
 }  // namespace sample
 }  // namespace yugen
