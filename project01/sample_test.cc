3f2ad1ce54e40454c876a4f5ce49b2808a7948ec
gankp <gankp@intotest.com>
2023-12-11 16:17:41 +0800

    Unit Test - Minus Function

--- project01/sample_test.cc
+++ project01/sample_test.cc
@@ -5,12 +5,6 @@
 namespace yugen {
 namespace sample {
 
-TEST(SampleTest, AddFn) {
-  Sample sample;
-  int result = sample.AddNumbers(1, 1);
-  EXPECT_EQ(result, 2);
-}
-
 TEST(SampleTest, MinusFn) {
   Sample sample;
   int result = sample.MinusNumbers(10, 9);
